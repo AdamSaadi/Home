0dcfcefc98b82257bea6fae9a67d61c7a564971d
Haiyan Saadi <haiyan25@hotmail.com>
2023-02-08 12:10:56 +0100

    correction and format in nav.js

--- src/component/sure/nav/nav.js
+++ src/component/sure/nav/nav.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import l1 from "../../../images/nav-logo.png"
+import l1 from "../../../images/Nav-logo.png"
 
 function App() {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -8,46 +8,46 @@ function App() {
     <div>
 
 
-<nav className="navbar">
-<Customlink href="/" ><img className="logo-pc" src={l1} alt="logo"/></Customlink>
-      <ul className="nav__links" >
-        <Customlink href="/" className="li-in-nav"> Home </Customlink>
-        <Customlink href="/Work" className="li-in-nav"> Work </Customlink>
-        <Customlink href="/Connect" className="li-in-nav"> Connect </Customlink>
-      </ul>
-      
+      <nav className="navbar">
+        <Customlink href="/" ><img className="logo-pc" src={l1} alt="logo" /></Customlink>
+        <ul className="nav__links" >
+          <Customlink href="/" className="li-in-nav"> Home </Customlink>
+          <Customlink href="/Work" className="li-in-nav"> Work </Customlink>
+          <Customlink href="/Connect" className="li-in-nav"> Connect </Customlink>
+        </ul>
 
 
-      <button className="hamburger" type="none" onClick={() => setShowNavbar(!showNavbar)}>
+
+        <button className="hamburger" type="none" onClick={() => setShowNavbar(!showNavbar)}>
           <span className="lala"></span>
           <span className="lala2"></span>
           <span className="lala3"></span>
-      </button>
+        </button>
 
-      <div className='mobile-navbar'>
+        <div className='mobile-navbar'>
 
 
-    
 
-      
-      {showNavbar && <div className="wow">
-        <ul className="Hm-elements" >
-          <Customlink href="/" ><li className="Hm-element" activeClassName="selected">Home</li></Customlink>
-          <Customlink href="/Work"><li className="Hm-element" activeClassName="selected">Work</li></Customlink>
-          <Customlink href="/Connect" ><li className="Hm-element" activeClassName="selected">Connect</li></Customlink>
-        </ul>
+
+
+          {showNavbar && <div className="wow">
+            <ul className="Hm-elements" >
+              <Customlink href="/" ><li className="Hm-element" activeClassName="selected">Home</li></Customlink>
+              <Customlink href="/Work"><li className="Hm-element" activeClassName="selected">Work</li></Customlink>
+              <Customlink href="/Connect" ><li className="Hm-element" activeClassName="selected">Connect</li></Customlink>
+            </ul>
+          </div>
+          }
         </div>
-}
-</div>
-    
+
         {showNavbar ? null : (
-          <img src={l1}  alt="Logo" className="logo"/>
+          <img src={l1} alt="Logo" className="logo" />
         )}
-        
-        </nav>
+
+      </nav>
 
     </div>
-    
+
   );
 }
 function Customlink({ href, children, ...props }) {
